729eceb4075396dd94e0b6b5f2164dbd3849f7b8
jakebtb <jake.estephan@beatthebomb.com>
2024-11-06 21:09:47 -0500

    Add files via upload

---
+++ Auto_Reset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+while true; do
+    BTBScreens="location"
+    # Check if the app is running
+    pid=$(ps aux | grep 'bash' | grep -v grep | awk '{print $2}')
+    BTBScreens_PATH="C:\Users\jacob\OneDrive\Desktop\TestingScripts\BTBScreens.sh"
+    echo "PID is $pid"
+
+    if [ -n "$pid" ]; then
+        # Check if the app is responding
+        if ! ps -p $pid > /dev/null; then
+            echo "$BTBScreens is not responding. Terminating the app..."
+            kill -9 $pid
+            echo "Restarting BTBscreens.sh..." 
+            bash "$BTBScreens_PATH"
+        else
+            echo "BTBScreens.sh is running and responding."
+        fi
+    else
+        echo "BTBScreens.sh is not running."
+        bash "$BTBScreens_PATH"
+    fi
+
+    # Wait for 30 seconds before checking again
+    sleep 30
+done
